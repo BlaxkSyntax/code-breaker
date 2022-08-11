c44dbedeb1e9c3fd96f2c231a4975d2635a25fad
blaxk.syntax <blaxk.syntax@gmail.com>
2022-08-11 20:03:31 +0200

    fixing mobile

--- qouterthing/android/app/src/main/kotlin/com/example/qouterthing/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.qouterthing
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- qouterthing/lib/repository/repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:qouterthing/home/model/homeMdel.dart';
-
-abstract class HomeRepo {
-  Future<List<Quotes>> getAllQoutes();
-  Future<Quotes> addQuote(String text, String name);
-}
